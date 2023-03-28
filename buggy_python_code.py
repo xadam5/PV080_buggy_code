b4871c499aa75766ebd3ddb2372adb2f5e45b939
Patrik Adam <xadam5@fi.muni.cz>
2023-03-28 11:23:44 +0200

    exec fixed

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,15 +25,15 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    #if urllib_version == '2' or urllib_version == '3':
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
+    if urllib_version == '2' or urllib_version == '3':
+        exec(f"import urllib{urllib_version} as urllib", globals())
+        # Fetch and print the requested URL
     
-    try: 
-        http = urllib.PoolManager()
-        r = http.request('GET', url)
-    except:
-        print('Exception')
+        try: 
+            http = urllib.PoolManager()
+            r = http.request('GET', url)
+        except:
+            print('Exception')
 
 
 def load_yaml(filename):
